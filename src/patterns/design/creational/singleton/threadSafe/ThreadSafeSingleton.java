aa5cef6f0bd9503b5055bdfe3bd241290fbfe06d
Atul Nale <aryannale@gmail.com>
2022-04-21 20:15:19 +0530

    create threadsafe singleton

---
+++ src/patterns/design/creational/singleton/threadSafe/ThreadSafeSingleton.java
@@ -0,0 +1,10 @@
+package patterns.design.creational.singleton.threadSafe;
+
+public class ThreadSafeSingleton {
+    private static ThreadSafeSingleton instance ;
+    private ThreadSafeSingleton(){}
+    public static synchronized ThreadSafeSingleton getInstance(){
+        if(instance == null) instance = new ThreadSafeSingleton();
+        return instance;
+    }
+}
